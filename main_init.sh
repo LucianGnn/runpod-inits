f043418a8685223e9157c88c78cc96d28679820a
LucianGnn <50239097+LucianGnn@users.noreply.github.com>
2025-06-07 09:04:22 +0300

    Update main_init.sh

--- main_init.sh
+++ main_init.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
 echo "Starting main_init.sh - Orchestrating custom setup."
-
-# Defineste calea catre directorul cu scripturile tale custom
-# Acesta ar trebui să fie '/workspace/runpod-inits', conform modificării Container Start Command
 CUSTOM_SCRIPTS_DIR="/workspace/runpod-inits"
-
-# --- Secțiune: Verificarea și Activarea Mediului Virtual ComfyUI ---
 source /workspace/ComfyUI/venv/bin/activate
-if [ $? -ne 0 ]; then
-    echo "ERROR: Failed to activate ComfyUI venv. Exiting main_init.sh."
-    exit 1
-fi
-echo "ComfyUI venv activated."
 
-echo "Running install_nodes.sh (Custom Nodes & Dependencies)..."
-# Folosim calea corectă către scriptul install_nodes.sh
-"${CUSTOM_SCRIPTS_DIR}"/install_nodes.sh
-if [ $? -ne 0 ]; then
-    echo "Warning: install_nodes.sh reported an error. Continuing initialization."
-fi
+# if [ $? -ne 0 ]; then
+#     echo "ERROR: Failed to activate ComfyUI venv. Exiting main_init.sh."
+#     exit 1
+# fi
+# echo "ComfyUI venv activated."
+
+# "${CUSTOM_SCRIPTS_DIR}"/install_nodes.sh
+# if [ $? -ne 0 ]; then
+#     echo "Warning: install_nodes.sh reported an error. Continuing initialization."
+# fi
 
 
 echo "Running download_models_fantasy_talking.sh (Model Download)..."
